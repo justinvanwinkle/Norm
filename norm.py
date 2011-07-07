d46b79546e7e9123878b6bdec5f84f492c071fee
Justin Van Winkle <justin.vanwinkle@gmail.com>
2011-07-07 00:54:39 -0700

    optimized to be 3x faster

--- norm.py
+++ norm.py
@@ -18,175 +18,92 @@ UPDATE_QT = b'u'
 INSERT_QT = b'i'
 
 SEP = '\n       '
+COLUMN_SEP = ',' + SEP
 
 
 class BogusQuery(Exception):
     pass
 
 
-class SQLCompiler(object):
-    def __init__(self, chain, query_type):
-        self.chain = chain
-        self.query_type = query_type
-        self.table = None
-        self.columns = []
-        self.from_ = []
-        self.where = []
-        self.having = None
-        self.group_by = None
-        self.order_by = None
-        self.limit = None
-        self.offset = None
-        self.set = []
-        self.extra = []
+def compile(chain, query_type):
+    table = None
+    columns = []
+    from_ = []
+    where = []
+    having = None
+    group_by = None
+    order_by = None
+    limit = None
+    offset = None
+    set_ = []
+    extra = []
 
-    def set_query_type(self, query_type):
-        self.query_type = query_type
-
-    def add_column(self, column_expr):
-        self.columns.append(column_expr)
-
-    def compile_columns(self):
-        if not self.columns:
-            raise BogusQuery('SELECT without any column expression.')
-        return 'SELECT ' + (',' + SEP).join(self.columns)
-
-    def add_from(self,
-                 table_expr,
-                 join,
-                 op,
-                 criteria):
-        if not join:
-            if self.from_:
-                self.from_[-1] += ','
-            self.from_.append(table_expr)
-        else:
-            self.from_[-1] += '\n  JOIN ' + table_expr
-            if op is not None:
-                self.from_[-1] += '\n       ' + op + ' ' + criteria
-
-    def compile_from(self):
-        if not self.from_:
-            return None
-        return '  FROM ' + SEP.join(self.from_)
-
-    def add_where(self, where_expr):
-        self.where.append(where_expr)
-
-    def compile_where(self):
-        if not self.where:
-            return None
-        return ' WHERE ' + (' AND' + SEP).join(self.where)
-
-    def set_having(self, having_expr):
-        self.having = having_expr
-
-    def compile_having(self):
-        if self.having is None:
-            return None
-        return 'HAVING ' + self.having
-
-    def set_group_by(self, group_by_expr):
-        self.group_by = group_by_expr
-
-    def compile_group_by(self):
-        if self.group_by is None:
-            return None
-        return 'GROUP BY ' + self.group_by
-
-    def set_order_by(self, order_by_expr):
-        self.order_by = order_by_expr
-
-    def compile_order_by(self):
-        if self.order_by is None:
-            return None
-        return 'ORDER BY ' + self.order_by
-
-    def set_limit(self, limit_expr):
-        self.limit = limit_expr
-
-    def compile_limit(self):
-        if self.limit is None:
-            return None
-        return ' LIMIT ' + self.limit
-
-    def set_offset(self, offset_expr):
-        self.offset = offset_expr
-
-    def compile_offset(self):
-        if self.offset is None:
-            return None
-        return 'OFFSET ' + self.offset
-
-    def add_extra(self, extra_expr):
-        self.extra.append(extra_expr)
-
-    def compile_extra(self):
-        if not self.extra:
-            return None
-        return '\n'.join(self.extra) if self.extra is not None else None
-
-    def set_table(self, table_expr):
-        self.table = table_expr
-
-    def compile_table(self):
-        return 'UPDATE ' + self.table
-
-    def add_set(self, set_expr):
-        self.set.append(set_expr)
-
-    def compile_set(self):
-        if not self.set:
-            return None
-        return '   SET ' + (',' + SEP).join(self.set)
-
-    def compile(self):
-        for op, options in self.chain:
-            if op == COLUMN:
-                self.add_column(*options)
-            elif op == WHERE:
-                self.add_where(*options)
-            elif op == FROM:
-                self.add_from(*options)
-            elif op == TABLE:
-                self.set_table(*options)
-            elif op == SET:
-                self.add_set(*options)
-            elif op == GROUP_BY:
-                self.set_group_by(*options)
-            elif op == ORDER_BY:
-                self.set_order_by(*options)
-            elif op == LIMIT:
-                self.set_limit(*options)
-            elif op == OFFSET:
-                self.set_offset(*options)
-            elif op == HAVING:
-                self.set_having(*options)
-            elif op == EXTRA:
-                self.add_extra(*options)
+    for op, option in chain:
+        if op == COLUMN:
+            columns.append(option)
+        elif op == WHERE:
+            where.append(option)
+        elif op == FROM:
+            expr, join, op, criteria = option
+            if not join:
+                if from_:
+                    from_[-1] += ','
+                from_.append(expr)
             else:
-                raise BogusQuery('There was a fatal error compiling query.')
+                from_[-1] += '\n  JOIN ' + expr
+                if op is not None:
+                    from_[-1] += '\n       ' + op + ' ' + criteria
+        elif op == TABLE:
+            table = option
+        elif op == SET:
+            set_.append(option)
+        elif op == GROUP_BY:
+            group_by = option
+        elif op == ORDER_BY:
+            order_by = option
+        elif op == LIMIT:
+            limit = option
+        elif op == OFFSET:
+            offset = option
+        elif op == HAVING:
+            having = option
+        elif op == EXTRA:
+            extra.append(option)
+        else:
+            raise BogusQuery('There was a fatal error compiling query.')
 
-        if self.query_type == SELECT_QT:
-            parts = [
-                self.compile_columns(),
-                self.compile_from(),
-                self.compile_where(),
-                self.compile_group_by(),
-                self.compile_having(),
-                self.compile_order_by(),
-                self.compile_limit(),
-                self.compile_offset(),
-                self.compile_extra()]
-        elif self.query_type == UPDATE_QT:
-            parts = [
-                self.compile_table(),
-                self.compile_set(),
-                self.compile_from(),
-                self.compile_where(),
-                self.compile_extra()]
+    query = ''
+    if query_type == SELECT_QT:
+        query += 'SELECT ' + COLUMN_SEP.join(columns)
+        if from_:
+            query += '\n  FROM ' + SEP.join(from_)
+        if where:
+            query += '\n WHERE ' + (' AND' + SEP).join(where)
+        if group_by is not None:
+            query += '\nGROUP BY ' + group_by
+        if having is not None:
+            query += '\nHAVING ' + having
+        if order_by is not None:
+            query += '\nORDER BY ' + order_by
+        if limit is not None:
+            query += '\n LIMIT ' + limit
+        if offset is not None:
+            query += '\nOFFSET ' + offset
+        if extra:
+            query += '\n'.join(extra)
+    elif query_type == UPDATE_QT:
+        query += 'UPDATE ' + table
+        if set_:
+            query += '\n   SET ' + (',' + SEP).join(set_)
+        if from_:
+            query += '\n  FROM ' + SEP.join(from_)
+        if where:
+            query += '\n WHERE ' + (' AND' + SEP).join(where)
+        if extra:
+            query += '\n'.join(extra)
 
-        return '\n'.join(part for part in parts if part is not None) + ';'
+    query += ';'
+    return query
 
 
 class Query(object):
@@ -224,8 +141,7 @@ class Query(object):
     @property
     def query(self):
         if self._query is None:
-            comp = SQLCompiler(self.build_chain(), self.query_type)
-            self._query = comp.compile()
+            self._query = compile(self.build_chain(), self.query_type)
         return self._query
 
 
@@ -234,13 +150,13 @@ class _SELECT_UPDATE(Query):
         # TODO: handle OR
         s = self.child()
         for stmt in args:
-            s.chain.append((WHERE, (stmt,)))
+            s.chain.append((WHERE, stmt))
         for column_name, value in kw.iteritems():
             column_name = unicode(column_name)
             bind_val_name = '%s_bind_%s' % (column_name, len(self.binds))
             self._binds[bind_val_name] = value
             expr = column_name + ' = %(' + bind_val_name + ')s'
-            s.chain.append((WHERE, (expr,)))
+            s.chain.append((WHERE, expr))
         return s
 
     def FROM(self, *args):
@@ -270,44 +186,44 @@ class SELECT(_SELECT_UPDATE):
     query_type = SELECT_QT
 
     def __init__(self, *args):
-        super(SELECT, self).__init__()
+        _SELECT_UPDATE.__init__(self)
 
         for stmt in args:
-            self.chain.append((COLUMN, (stmt,)))
+            self.chain.append((COLUMN, stmt))
 
     def SELECT(self, *args):
         s = self.child()
         for stmt in args:
-            s.chain.append((COLUMN, (stmt,)))
+            s.chain.append((COLUMN, stmt))
         return s
 
     def HAVING(self, stmt):
         s = self.child()
-        s.chain.append((HAVING, (stmt,)))
+        s.chain.append((HAVING, stmt))
         return s
 
     def ORDER_BY(self, stmt):
         s = self.child()
-        s.chain.append((ORDER_BY, (stmt,)))
+        s.chain.append((ORDER_BY, stmt))
         return s
 
     def GROUP_BY(self, stmt):
         s = self.child()
-        s.chain.append((GROUP_BY, (stmt,)))
+        s.chain.append((GROUP_BY, stmt))
         return s
 
     def LIMIT(self, stmt):
         if isinstance(stmt, (int, long)):
             stmt = unicode(stmt)
         s = self.child()
-        s.chain.append((LIMIT, (stmt,)))
+        s.chain.append((LIMIT, stmt))
         return s
 
     def OFFSET(self, stmt):
         if isinstance(stmt, (int, long)):
             stmt = unicode(stmt)
         s = self.child()
-        s.chain.append((OFFSET, (stmt,)))
+        s.chain.append((OFFSET, stmt))
         return s
 
 
@@ -318,18 +234,18 @@ class UPDATE(_SELECT_UPDATE):
         super(UPDATE, self).__init__()
 
         if table is not None:
-            self.chain.append((TABLE, (table,)))
+            self.chain.append((TABLE, table))
 
     def SET(self, *args, **kw):
         s = self.child()
         for stmt in args:
-            self.chain.append((SET, (stmt,)))
+            self.chain.append((SET, stmt))
 
         for column_name, value in kw.iteritems():
             bind_name = column_name + '_bind'
             self._binds[bind_name] = value
             expr = unicode(column_name) + ' = %(' + bind_name + ')s'
-            s.chain.append((SET, (expr,)))
+            s.chain.append((SET, expr))
         return s
 
     def RETURNING(self, *args):
