540c6a9fb9c717e803ef8fa3fe9aba718842bbfe
Justin Van Winkle <jvanwinkle@earnup.com>
2019-10-07 20:10:40 -0700

    add myssql connector

---
+++ norm/norm_myssql_connector.py
@@ -0,0 +1,35 @@
+from .norm import SELECT
+from .norm import INSERT
+from .norm import UPDATE
+from .norm import DELETE
+from .connection import ConnectionFactory
+from .connection import ConnectionProxy
+from .connection import CursorProxy
+
+
+class MY_CON_INSERT(INSERT):
+    pass
+
+
+class MY_CON_SELECT(SELECT):
+    pass
+
+
+class MY_CON_UPDATE(UPDATE):
+    pass
+
+
+class MY_CON_DELETE(DELETE):
+    pass
+
+
+class MY_CON_CursorProxy(CursorProxy):
+    pass
+
+
+class MY_CON_ConnectionProxy(ConnectionProxy):
+    cursor_proxy = MY_CON_CursorProxy
+
+
+class MY_CON_ConnectionFactory(ConnectionFactory):
+    connection_proxy = MY_CON_ConnectionProxy
