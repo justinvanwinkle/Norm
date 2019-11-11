14ef4dc95c706f0cedf387b8c9750b1b81209ca3
Justin Van Winkle <jvanwinkle@earnup.com>
2019-11-11 14:52:07 -0800

    add pymssql

---
+++ norm/norm_pymssql.py
@@ -0,0 +1,36 @@
+
+from norm.norm import SELECT
+from norm.norm import INSERT
+from norm.norm import UPDATE
+from norm.norm import DELETE
+from norm.connection import ConnectionFactory
+from norm.connection import ConnectionProxy
+from norm.connection import CursorProxy
+
+
+class PYMSSQL_INSERT(INSERT):
+    pass
+
+
+class PYMSSQL_SELECT(SELECT):
+    pass
+
+
+class PYMSSQL_UPDATE(UPDATE):
+    pass
+
+
+class PYMSSQL_DELETE(DELETE):
+    pass
+
+
+class PYMSSQL_CursorProxy(CursorProxy):
+    pass
+
+
+class PYMSSQL_ConnectionProxy(ConnectionProxy):
+    cursor_proxy = PYMSSQL_CursorProxy
+
+
+class PYMSSQL_ConnectionFactory(ConnectionFactory):
+    connection_proxy = PYMSSQL_ConnectionProxy
